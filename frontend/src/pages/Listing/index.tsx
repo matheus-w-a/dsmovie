ea52fc2718f5ecc7dd005cf3284595d7b6a0b299
Matheus Andrade <matheus.andrade@gesplan.com.br>
2022-03-09 14:00:02 -0300

    moviecard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,17 @@
+import { MovieCard } from "components/MovieCard";
 import { Pagination } from "components/Pagination";
 
 export function Listing() {
   return (
-    <Pagination />
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-6 mb-3">
+            <MovieCard />        
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
